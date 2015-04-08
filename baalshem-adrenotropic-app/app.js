d050d66d5ff45ae82767882f9d3f6dde72d5f620
Emily Hittle <emilyliza@gmail.com>
2015-04-07 17:40:57 -0700

    Update readme

--- baalshem-adrenotropic-app/app.js
+++ baalshem-adrenotropic-app/app.js
@@ -12,43 +12,71 @@ var db = require("./models");
 
 app.get('/articles', function(req,res) {
   console.log("GET /articles");
-  res.send("Set up a response for this route!");
+  db.Article.findAll({include: db.Author}).then(function (articles) {
+      res.render("articles/index", {articlesList: articles});
+      });
 });
 
 app.get('/articles/new', function(req,res) {
-  res.render('articles/new');
+  db.Author.findAll().then(function(authors){
+  res.render('articles/new', {ejsAuthors: authors});
+  });
 });
 
 app.post('/articles', function(req,res) {
-  console.log(req.body);
-  res.send("Set up a response for this route!");
+  var article = req.body.article;
+	
+	db.Article.create (article)
+	.then(function (newArticle) {
+	 res.redirect("/articles");
+  });
 });
 
 app.get('/articles/:id', function(req, res) {
-  res.send("Set up a response for this route!");
+  var id = req.params.id;	 
   
-})
+  db.Article.find(id)
+  .then(function(foundArticle) {
+  res.render("articles/article", {articleToDisplay: foundArticle});
+
+  });
+});
 
 // Fill in these author routes!
 app.get('/authors', function(req, res) {
-	console.log("GET /authors")
-	res.send("Set up a response for this route!");
+	db.Author.findAll().then(function (authors) {
+    res.render("authors/index", {ejsAuthors: authors});
+    });
 
 });
 
 app.get('/authors/new', function(req, res) {
-	console.log("GET /authors/new")
-	res.send("Set up a response for this route!");
+	res.render('authors/new');
 });
 
 app.post('/authors', function(req, res) {
 	console.log(req.body);
-	res.send("Set up a response for this route!");
+	var author = req.body.author;
+	
+	db.Author.create (author)
+	.then(function (dbAuthor) {
+		res.redirect("/authors");
+	});
 });
 
 app.get('/authors/:id', function(req, res) {
-	console.log("GET /authors/:id")
-	res.send("Set up a response for this route!");
+  console.log("GET /authors/:id")
+  var id = req.params.id;	 
+  
+  db.Author.find(id)
+  .then(function(author) {
+  	db.Article.findAll({where: {AuthorId: id}})
+  	.then(function(foundArticles){
+  		console.log("These are the articles");
+  		console.log(foundArticles.dataValues);
+  		res.render("authors/author", {ejsAuthor: author, authorArticles: foundArticles});
+	});
+  });
 });
 
 app.get('/', function(req,res) {
@@ -63,8 +91,10 @@ app.get('/contact', function(req,res) {
   res.render('site/contact');
 });
 
-app.listen(3000, function() {
-	var msg = "* Listening on Port 3000 *";
+
+db.sequelize.sync().then(function(){
+	app.listen(3000, function() {
+		var msg = "* Listening on Port 3000 *";
 
 	// Just for fun... what's going on in this code?
 	/*
@@ -75,7 +105,8 @@ app.listen(3000, function() {
 	 *	**************************
 	 *
 	*/
-	console.log(Array(msg.length + 1).join("*"));
-	console.log(msg);
-	console.log(Array(msg.length + 1).join("*"));
+		console.log(Array(msg.length + 1).join("*"));
+		console.log(msg);
+		console.log(Array(msg.length + 1).join("*"));
+	});
 });
